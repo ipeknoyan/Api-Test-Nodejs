d31c8519128aa20d13d9cfe30bd6706c5db9faeb
ipeknoyan <noyanipek@gmail.com>
2022-09-04 00:09:00 +0300

    http get test cases are created

--- test/users.js
+++ test/users.js
@@ -1,27 +1,53 @@
 import supertest from "supertest";
 const request= supertest('http://ptsv2.com/t/fu807-1554722621');
 
-import { expect } from 'chai';
+import { expect, should } from 'chai';
 
-var axios = require('axios')
 var response;
+var data;
 
 describe('http GET Cases', () => {
 
     beforeEach(() => {
         return request.get('/post').then((res) => {
             console.log("http get is requested");
-            res.body = JSON.parse(res.text)
+            const obj = JSON.parse(res.text);
             response = res;
+            data= obj;
         });
 	})
  
 
+    it('GetRequestSuccessfully', () => {
+        expect(response.status).to.equal(200);
+        console.log("Response code is 200");
+    });
+
     it('GetData', () => {
+        response.body = JSON.parse(response.text)
         expect(response.body.data).to.not.be.null;
         console.log("Response data is not null");
     });
 
-   
+    it('CheckKeys', () => {
+        response.body = JSON.parse(response.text)
+        expect(response.body).to.have.keys(['username',"password","targetUrl"]);
+        console.log("Response data includes correct keys");
+    });
+
+    it('CheckUsername', () => {
+        
+        expect(data.username).to.be.equal('automate');
+        console.log("Username is automate");
+        
+    });
+
+    it('CheckPassword', () => {
+        
+        expect(data.password).to.be.equal('everything');
+        console.log("Password is everything");
+        
+    });
+
  });
 
