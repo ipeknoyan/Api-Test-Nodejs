bddba7370c9ca74e1bfe13b44ae777a4d6fdceb8
ipeknoyan <noyanipek@gmail.com>
2022-09-06 19:18:25 +0300

    comments are added

--- test/users.js
+++ test/users.js
@@ -3,13 +3,15 @@ const request= supertest('http://ptsv2.com/');
 
 import { expect, should } from 'chai';
 
+//Create variables
 var response;
 var data;
 var username;
 var password;
 
-describe('http GET Cases', () => {
-
+//This test suit includes GET Cases
+describe('GET Cases', () => {
+    //Request api for each test case
     beforeEach(() => {
         return request.get('t/fu807-1554722621/post').then((res) => {
             console.log("http get is requested");
@@ -21,27 +23,31 @@ describe('http GET Cases', () => {
         });
 	})
  
-
+    //This case checks if GET API is successful
     it('GetRequestIsSuccessful', () => {
         expect(response.status).to.equal(200);
         console.log("Response code is 200");
     });
 
+    //This case checks if response data not null
     it('GetResponseIsNotNull', () => {
         expect(data).to.not.be.null;
         console.log("Response data is not null");
     });
 
+     //This case checks if response data includes "username", "password"and "targetUrl" keys
     it('ValidateKeysOfGetRequest', () => {
         expect(data).to.have.keys(['username',"password","targetUrl"]);
         console.log("Response data includes correct keys");
     });
-
+    
+    //This case checks if "username=automate"
     it('ValidateUsername', () => {   
         expect(data.username).to.be.equal('automate');
         console.log("Username is automate");        
     });
 
+     //This case checks if "password=everything"
     it('validatePassword', () => {  
         expect(data.password).to.be.equal('everything');
         console.log("Password is everything");    
@@ -49,8 +55,9 @@ describe('http GET Cases', () => {
 
  });
 
- describe('http POST Cases', () => {
-
+ //This test suit includes POST Cases
+ describe('POST Cases', () => {
+    //Request api for each test case
     beforeEach(() => {
         return request
              .post('t/7ty82-1554722743/post')
@@ -63,16 +70,19 @@ describe('http GET Cases', () => {
         
     })
 
+    //This case checks if POST API is successful
     it('PostRequestIsSuccessful', () => {
         expect(response.status).to.equal(200);
         console.log("Response code is 200");
     });
 
+    //This case checks if response data not null
     it('PostResponseIsNotNull', () => {
         expect(data).to.not.be.null;
         console.log("Response data is not null");
     });
 
+    //This case checks if response data includes "ip" and "token" keys
     it('ValidateKeysOfPostRequest', () => {
         expect(data).to.have.keys(['ip','token']);
         console.log("Response data includes correct keys");
